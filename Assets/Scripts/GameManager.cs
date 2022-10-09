4ffaae342eb0ed275c7859cbb1f6ecdb5e743455
Vincenzo Masciale <v.masciale@grifomultimedia.it>
2022-10-09 23:33:11 +0200

    BL Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    // BL
+    List<Cocktail> _cocktails = new List<Cocktail>();
+    Cocktail _cocktail;
 
-    public List<Cocktail> _cocktails = new List<Cocktail>();
+    // UI
+    public Image _baseIngredient;
+    public Image _flavoringIngredient;
+    public List<Sprite> _baseIngredients;
+    public List<Sprite> _flavoringIngredients;
 
     // Start is called before the first frame update
     void Start()
     {
         Init();
-        Debug.Log("Base: " + RandomCocktail().Base.GetType().Name);
-        Debug.Log("Flavoring: " + RandomCocktail().Flavoring.GetType().Name);
+        _cocktail = RandomCocktail();
+        Debug.Log("Cocktail:" + _cocktail.GetType().Name);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void Init()
@@ -26,7 +34,7 @@ public class GameManager : MonoBehaviour
         _cocktails.Add(new Martini());
         _cocktails.Add(new MoscowMule());
 
-        foreach(Cocktail ct in _cocktails)
+        foreach (Cocktail ct in _cocktails)
         {
             ct.Create();
             ct.Create();
@@ -36,5 +44,25 @@ public class GameManager : MonoBehaviour
     public Cocktail RandomCocktail()
     {
         return _cocktails[Random.Range(0, _cocktails.Count)];
+    }
+
+    public void CheckComposition()
+    {
+        if (_baseIngredient.GetComponent<Image>().sprite.name.Equals(_cocktail.Base.GetType().Name.ToLower())
+            &&
+            _flavoringIngredient.GetComponent<Image>().sprite.name.Equals(_cocktail.Flavoring.GetType().Name.ToLower()))
+            Debug.Log("OK");
+        else
+            Debug.Log("ERROR");
+    }
+
+    public void SwitchBase()
+    {
+        _baseIngredient.GetComponent<Image>().sprite = _baseIngredients[Random.Range(0, _baseIngredients.Count)];
+    }
+
+    public void SwitchFlavoring()
+    {
+        _flavoringIngredient.GetComponent<Image>().sprite = _flavoringIngredients[Random.Range(0, _flavoringIngredients.Count)];
     }
 }
